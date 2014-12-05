59ffe57ac5330fc5ed9b0f664e36518f55c279b9
Nikolay Yakovlev <nyakovlev@mirantis.com>
2014-12-05 12:31:47 +0200

    * Copyright fixes * DB opts removed * Docstring fix
    Change-Id: Iea14504e79221a4b13554db3b196dea4a9438515

--- designate/backend/impl_infoblox/records/base.py
+++ designate/backend/impl_infoblox/records/base.py
@@ -1,18 +1,8 @@
 # Copyright 2014 Mirantis
 #
-# Author: Nikolay Yakovlev <nyakovlev@mirantis.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# This work is licensed under a Creative Commons Attribution 3.0
+# Unported License.
+# http://creativecommons.org/licenses/by/3.0/legalcode
 
 from abc import abstractmethod
 from abc import ABCMeta
